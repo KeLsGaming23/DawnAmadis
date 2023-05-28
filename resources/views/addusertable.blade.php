6bbecc812ee4a23676b60002d6ccab079ff0345f
KelsGaming <alcorizamichael@gmail.com>
2023-05-28 11:56:11 +0800

    Change alert to dismissable

--- resources/views/addusertable.blade.php
+++ resources/views/addusertable.blade.php
@@ -5,9 +5,13 @@
         <div class="col-12">
           <div class="card card-frame">
             @if(session('success'))
-            <div class="alert alert-success" role="alert">
-                <strong>Success!</strong> {{ session('success') }}
-            </div>
+              <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <span class="alert-icon"><i class="ni ni-like-2"></i></span>
+                <span class="alert-text"><strong>Success!</strong> {{ session('success') }}</span>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
             @endif
             <div class="card-body">
               <form action="{{ route('store.user') }}" method="POST">
