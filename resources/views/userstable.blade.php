996ab4ba678d0adda435bf34212970cc5b300137
KelsGaming <alcorizamichael@gmail.com>
2023-05-28 22:08:07 +0800

    Trying to show Image files

--- resources/views/userstable.blade.php
+++ resources/views/userstable.blade.php
@@ -26,7 +26,7 @@
                       <td>
                         <div class="d-flex px-2 py-1">
                           <div>
-                            <img src="../assets/img/team-2.jpg" class="avatar avatar-sm me-3" alt="user1">
+                            <img src="{{asset('/image/student-profile/1767146127013664.jpg')}}" class="avatar avatar-sm me-3" alt="user1">
                           </div>
                           <div class="d-flex flex-column justify-content-center">
                             <h6 class="mb-0 text-sm">{{ $user->name }}</h6>
