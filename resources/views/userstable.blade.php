ece4173ef5152de5c04c5657eb3aba9ad3c8b21c
KelsGaming <alcorizamichael@gmail.com>
2023-05-28 19:26:44 +0800

    Add Delete Button

--- resources/views/userstable.blade.php
+++ resources/views/userstable.blade.php
@@ -45,8 +45,11 @@
                         <span class="text-secondary text-xs font-weight-bold">{{ $user->random_token }}</span>
                       </td>
                       <td class="align-middle">
-                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs p-2" data-toggle="tooltip" data-original-title="Edit user">
                           Edit
+                        </a>
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs p-2" data-toggle="tooltip" data-original-title="Edit user">
+                          Delete
                         </a>
                       </td>
                     </tr>
