bafeddb32ae71f632b2bc1a1cbdeeba1e652a50c
KelsGaming <alcorizamichael@gmail.com>
2023-05-28 22:23:12 +0800

    Trying to show Image files

--- resources/views/userstable.blade.php
+++ resources/views/userstable.blade.php
@@ -26,7 +26,12 @@
                       <td>
                         <div class="d-flex px-2 py-1">
                           <div>
-                            <img src="{{ $user->studentBasicInformation->profile_picture }}" class="avatar avatar-sm me-3" alt="user1">
+                          @if($user->studentBasicInformation)
+                            <img src="{{ asset($user->studentBasicInformation->profile_picture) }}" class="avatar avatar-sm me-3" alt="user1">
+                          @else
+                            <!-- Handle the case where studentBasicInformation is null -->
+                            <img src="{{ asset('path-to-default-profile-photo.jpg') }}" class="avatar avatar-sm me-3" alt="default-user">
+                          @endif
                           </div>
                           <div class="d-flex flex-column justify-content-center">
                             <h6 class="mb-0 text-sm">{{ $user->name }}</h6>
