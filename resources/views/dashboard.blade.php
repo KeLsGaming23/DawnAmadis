fc7893444226720acdc06624febd4022afd93a05
KelsGaming <alcorizamichael@gmail.com>
2023-05-28 12:29:23 +0800

    Add display flex for right spacing

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -202,11 +202,11 @@
                       <div class="text-center">
                         <p class="text-xs font-weight-bold mb-0">Action</p>
                         <div class="d-flex justify-content-center mb-3">
-                          <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Show Archive  ">
-                            Show Archive
+                          <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Show Archive">
+                            Show Archive   
                           </a>
                           <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit School Year">
-                            Edit School Year
+                              Edit School Year
                           </a>
                         </div>
                       </div>
