89760ac74beac308dafb157476cc67023cb05ee3
KelsGaming <alcorizamichael@gmail.com>
2023-05-28 21:32:20 +0800

    Finalizing the Student Form

--- app/Models/StudentBasicInformation.php
+++ app/Models/StudentBasicInformation.php
@@ -11,6 +11,18 @@ class StudentBasicInformation extends Model
 
     protected $table = 'student_basic_informations';
 
-    protected $fillable = ['last_name', 'first_name', 'middle_name', 'users_id', 'school_years_id'];
+    protected $fillable = [
+        'last_name', 
+        'first_name', 
+        'middle_name', 
+        'users_id', 
+        'school_years_id',
+        'old_student',
+        'gender',
+        'birth_date',
+        'place_of_birth',
+        'grade',
+        'profile_picture'
+    ];
 
 }
