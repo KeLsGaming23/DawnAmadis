6e6c760d2566f7e7ddee2c0fc9df409386f92cbd
KelsGaming <alcorizamichael@gmail.com>
2023-05-28 16:28:04 +0800

    Edit Dashboard view to display all school year created

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -180,38 +180,42 @@
             <div class="table-responsive">
               <table class="table align-items-center ">
                 <tbody>
-                  <tr>
-                    <td class="w-30">
-                      <div class="d-flex px-2 py-1 align-items-center">
-                        <div>
-                          <svg class="text-dark" width="16px" height="16px" viewBox="0 0 46 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <title>customer-support</title> <g id="Basic-Elements" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Rounded-Icons" transform="translate(-1717.000000, -291.000000)" fill="#FFFFFF" fill-rule="nonzero"> <g id="Icons-with-opacity" transform="translate(1716.000000, 291.000000)"> <g id="customer-support" transform="translate(1.000000, 0.000000)"> <path class="color-background" d="M45,0 L26,0 C25.447,0 25,0.447 25,1 L25,20 C25,20.379 25.214,20.725 25.553,20.895 C25.694,20.965 25.848,21 26,21 C26.212,21 26.424,20.933 26.6,20.8 L34.333,15 L45,15 C45.553,15 46,14.553 46,14 L46,1 C46,0.447 45.553,0 45,0 Z" id="Path" opacity="0.59858631"></path> <path class="color-foreground" d="M22.883,32.86 C20.761,32.012 17.324,31 13,31 C8.676,31 5.239,32.012 3.116,32.86 C1.224,33.619 0,35.438 0,37.494 L0,41 C0,41.553 0.447,42 1,42 L25,42 C25.553,42 26,41.553 26,41 L26,37.494 C26,35.438 24.776,33.619 22.883,32.86 Z" id="Path"></path> <path class="color-foreground" d="M13,28 C17.432,28 21,22.529 21,18 C21,13.589 17.411,10 13,10 C8.589,10 5,13.589 5,18 C5,22.529 8.568,28 13,28 Z" id="Path"></path> </g> </g> </g> </g> </svg>
-                        </div>
-                        <div class="ms-4">
-                          <p class="text-xs font-weight-bold mb-0">School Year ID</p>
-                          <h6 class="text-sm mb-0">1</h6>
-                        </div>
+                @foreach ($schoolYears as $schoolYear)
+                <tr>
+                  <td class="w-30">
+                    <div class="d-flex px-2 py-1 align-items-center">
+                      <div>
+                        <svg class="text-dark" width="16px" height="16px" viewBox="0 0 46 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <title>customer-support</title> <g id="Basic-Elements" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Rounded-Icons" transform="translate(-1717.000000, -291.000000)" fill="#FFFFFF" fill-rule="nonzero"> <g id="Icons-with-opacity" transform="translate(1716.000000, 291.000000)"> <g id="customer-support" transform="translate(1.000000, 0.000000)"> <path class="color-background" d="M45,0 L26,0 C25.447,0 25,0.447 25,1 L25,20 C25,20.379 25.214,20.725 25.553,20.895 C25.694,20.965 25.848,21 26,21 C26.212,21 26.424,20.933 26.6,20.8 L34.333,15 L45,15 C45.553,15 46,14.553 46,14 L46,1 C46,0.447 45.553,0 45,0 Z" id="Path" opacity="0.59858631"></path> <path class="color-foreground" d="M22.883,32.86 C20.761,32.012 17.324,31 13,31 C8.676,31 5.239,32.012 3.116,32.86 C1.224,33.619 0,35.438 0,37.494 L0,41 C0,41.553 0.447,42 1,42 L25,42 C25.553,42 26,41.553 26,41 L26,37.494 C26,35.438 24.776,33.619 22.883,32.86 Z" id="Path"></path> <path class="color-foreground" d="M13,28 C17.432,28 21,22.529 21,18 C21,13.589 17.411,10 13,10 C8.589,10 5,13.589 5,18 C5,22.529 8.568,28 13,28 Z" id="Path"></path> </g> </g> </g> </g> </svg>
                       </div>
-                    </td>
-                    <td>
-                      <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">School Year</p>
-                        <h6 class="text-sm mb-0">SY 2023 - 2024</h6>
+                      <div class="ms-4">
+                        <p class="text-xs font-weight-bold mb-0">School Year ID</p>
+                        <h6 class="text-sm mb-0">{{ $schoolYear->id }}</h6>
                       </div>
-                    </td>
-                    <td>
-                      <div class="text-center">
-                        <p class="text-xs font-weight-bold mb-0">Action</p>
-                        <div class="d-flex justify-content-center mb-3">
-                          <a href="javascript:;" class="text-secondary font-weight-bold text-xs p-2" data-toggle="tooltip" data-original-title="Show Archive">
-                            Show Archive
-                          </a>
-                          <a href="javascript:;" class="text-secondary font-weight-bold text-xs p-2" data-toggle="tooltip" data-original-title="Edit School Year">
-                            Edit School Year
-                          </a>
-                        </div>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="text-center">
+                      <p class="text-xs font-weight-bold mb-0">School Year</p>
+                      <h6 class="text-sm mb-0">{{ $schoolYear->school_year }}</h6>
+                    </div>
+                  </td>
+                  <td>
+                    <div class="text-center">
+                      <p class="text-xs font-weight-bold mb-0">Action</p>
+                      <div class="d-flex justify-content-center mb-3">
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs p-2" data-toggle="tooltip" data-original-title="Show Archive">
+                          Show Archive
+                        </a>
+                        <a href="javascript:;" class="text-secondary font-weight-bold text-xs p-2" data-toggle="tooltip" data-original-title="Edit School Year">
+                          Edit School Year
+                        </a>
                       </div>
-                    </td>
-                  </tr>
+                    </div>
+                  </td>
+                </tr>
+                @endforeach
+
+                 
                     <!-- Modal for Adding School year Archive -->
                     <div class="col-md-4">
                       <div class="modal fade" id="modal-form" tabindex="-1" role="dialog" aria-labelledby="modal-form" aria-hidden="true">
