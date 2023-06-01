cc6459722bb68b28243c810951dfefeb17010c39
KelsGaming <alcorizamichael@gmail.com>
2023-06-01 22:18:23 +0800

    show name in the modal title section

--- resources/views/userstable.blade.php
+++ resources/views/userstable.blade.php
@@ -126,7 +126,7 @@
             <p class="text-xs text-secondary mb-0">Birth Date: ${user.birth_date}</p>
             <p class="text-xs text-secondary mb-0">Place of Birth: ${user.place_of_birth}</p>
             <p class="text-xs text-secondary mb-0">Grade: ${user.grade}</p>
-            <img class="avatar avatar-sm me-3" src="${user.profile_picture}" alt="Profile Picture">
+            <img src="${user.profile_picture}" alt="Profile Picture" style="border-radius: 50%; width: 200px; height: 200px;">
             <!-- Add more user details here -->
             </div>
           </div>
