02785127f6e0312ffbb6b958dcdc4922ae4b732a
KelsGaming <alcorizamichael@gmail.com>
2023-06-02 15:42:20 +0800

    Change input field dynamically when selecting role

--- resources/views/addusertable.blade.php
+++ resources/views/addusertable.blade.php
@@ -26,25 +26,60 @@
                       <option value="Staff">Staff</option>
                     </select>
                   </div>
-                  <div class="col-lg-6">
-                    <label for="grade">Grade</label>
-                    <select class="form-control" id="grade" name="grade">
-                      <option value="Kinder">Kinder</option>
-                      <option value="Prep">Prep</option>
-                      <option value="Grade 1">Grade 1</option>
-                      <option value="Grade 2">Grade 2</option>
-                      <option value="Grade 3">Grade 3</option>
-                      <option value="Grade 4">Grade 4</option>
-                      <option value="Grade 5">Grade 5</option>
-                      <option value="Grade 6">Grade 6</option>
-                      <option value="Grade 7">Grade 7</option>
-                      <option value="Grade 8">Grade 8</option>
-                      <option value="Grade 9">Grade 9</option>
-                      <option value="Grade 10">Grade 10</option>
+                </div>
+                <div class="form-group row" id="studentFields">
+                  <div class="form-group">
+                    <div class="col-lg-6">
+                      <label for="grade">Grade</label>
+                      <select class="form-control" id="grade" name="grade">
+                        <option value="Kinder">Kinder</option>
+                        <option value="Prep">Prep</option>
+                        <option value="Grade 1">Grade 1</option>
+                        <option value="Grade 2">Grade 2</option>
+                        <option value="Grade 3">Grade 3</option>
+                        <option value="Grade 4">Grade 4</option>
+                        <option value="Grade 5">Grade 5</option>
+                        <option value="Grade 6">Grade 6</option>
+                        <option value="Grade 7">Grade 7</option>
+                        <option value="Grade 8">Grade 8</option>
+                        <option value="Grade 9">Grade 9</option>
+                        <option value="Grade 10">Grade 10</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="col-lg-3">
+                    <label for="first_name">First Name</label>
+                    <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Juan">
+                  </div>
+                  <div class="col-lg-3">
+                    <label for="middle_name">Middle Name</label>
+                    <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Middle Name">
+                  </div>
+                  <div class="col-lg-3">
+                    <label for="last_name">Last Name</label>
+                    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Dela Cruz">
+                  </div>
+                  <div class="col-lg-3">
+                    <label for="gender">Gender</label>
+                    <select class="form-control" id="gender" name="gender">
+                      <option value="Male">Male</option>
+                      <option value="Female">Female</option>
                     </select>
                   </div>
+                  <div class="form-group row">
+                    <div class="col-lg-6">
+                      <div class="form-group">
+                        <label for="birth_date" class="form-control-label">Birth Date</label>
+                        <input class="form-control" type="date" value="2023-01-01" id="birth_date" name="birth_date">
+                      </div>
+                    </div>
+                    <div class="col-lg-6">
+                      <label for="place_of_birth">Place of Birth</label>
+                      <input type="text" class="form-control" id="place_of_birth" name="place_of_birth" placeholder="Juan">
+                    </div>
+                  </div>
                 </div>
-
+                
                 <div class="form-group row" id="parentFields" style="display: none;">
                   <div class="col-lg-6">
                     <label for="fathers_name">Father's Name</label>
@@ -65,40 +100,6 @@
                   <div class="col-lg-6">
                     <label for="contact_no">Contact No.</label>
                     <input type="text" class="form-control" id="contact_no" name="contact_no" placeholder="Contact No.">
-                  </div>
-                </div>
-
-                <div class="form-group row" id="studentFields">
-                  <div class="col-lg-3">
-                    <label for="first_name">First Name</label>
-                    <input type="text" class="form-control" id="first_name" name="first_name" placeholder="Juan">
-                  </div>
-                  <div class="col-lg-3">
-                    <label for="middle_name">Middle Name</label>
-                    <input type="text" class="form-control" id="middle_name" name="middle_name" placeholder="Middle Name">
-                  </div>
-                  <div class="col-lg-3">
-                    <label for="last_name">Last Name</label>
-                    <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Dela Cruz">
-                  </div>
-                  <div class="col-lg-3">
-                    <label for="gender">Gender</label>
-                    <select class="form-control" id="gender" name="gender">
-                      <option value="Male">Male</option>
-                      <option value="Female">Female</option>
-                    </select>
-                  </div>
-                </div>
-                <div class="form-group row">
-                  <div class="col-lg-6">
-                    <div class="form-group">
-                      <label for="birth_date" class="form-control-label">Birth Date</label>
-                      <input class="form-control" type="date" value="2023-01-01" id="birth_date" name="birth_date">
-                    </div>
-                  </div>
-                  <div class="col-lg-6">
-                    <label for="place_of_birth">Place of Birth</label>
-                    <input type="text" class="form-control" id="place_of_birth" name="place_of_birth" placeholder="Juan">
                   </div>
                 </div>
                 <div class="form-group row">
