595636b9c0d130facb2cae4f1908c7709f2a28e1
KelsGaming <alcorizamichael@gmail.com>
2023-06-08 11:53:14 +0800

    Fix html table structure

--- resources/views/schoolyeararchive.blade.php
+++ resources/views/schoolyeararchive.blade.php
@@ -20,10 +20,10 @@
                   <tbody>
                   @foreach ($students as $student)
                     <tr>
-                        <td>{{ $student->last_name }}</td>
-                        <td>{{ $student->first_name }}</td>
-                        <td>{{ $student->middle_name }}</td>
-                        <td>{{ $student->grade }}</td>
+                        <td class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">{{ $student->last_name }}</td>
+                        <td class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">{{ $student->first_name }}</td>
+                        <td class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">{{ $student->middle_name }}</td>
+                        <td class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">{{ $student->grade }}</td>
                     </tr>
                   @endforeach
                   </tbody>
