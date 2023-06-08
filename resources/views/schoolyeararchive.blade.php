af89eb0bcc3a04cfa70cca9bf2330c23fd91a525
KelsGaming <alcorizamichael@gmail.com>
2023-06-08 11:42:21 +0800

    Show School Years Id

--- resources/views/schoolyeararchive.blade.php
+++ resources/views/schoolyeararchive.blade.php
@@ -4,7 +4,7 @@
         <div class="col-12">
           <div class="card mb-4">
             <div class="card-header pb-0">
-              <h6 class="">Current School Year: {{ $currentSchoolYear->school_year }}</h6>
+              <h6 class="">Current School Year: {{ $student->school_years_id }}</h6>
             </div>
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
