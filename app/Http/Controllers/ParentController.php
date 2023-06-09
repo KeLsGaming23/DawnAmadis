8f4b9b26a8061036138fa93566a1bdbe795b2724
KelsGaming <alcorizamichael@gmail.com>
2023-06-09 17:16:43 +0800

    Fix syntax error

--- app/Http/Controllers/ParentController.php
+++ app/Http/Controllers/ParentController.php
@@ -19,7 +19,7 @@ class ParentController extends Controller
             $randomToken = Str::random(6);
         }
         $user = User::create([
-            'role' => $request->role,
+            'role' => 'Parent',
             'random_token' => $randomToken,
             'created_at' => Carbon::now()
         ]);
