5519fc7191bf9962e193c227d2fed68c30298320
KelsGaming <alcorizamichael@gmail.com>
2023-06-09 17:52:04 +0800

    Create Role Middleware to handle role redirect

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,16 @@
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RoleMiddleware
+{
+    public function handle(Request $request, Closure $next, $role)
+    {
+        if (! $request->user()->hasRole($role)) {
+            abort(403, 'Unauthorized');
+        }
+
+        return $next($request);
+    }
+}
