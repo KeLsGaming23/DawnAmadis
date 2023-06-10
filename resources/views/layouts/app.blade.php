073c18154a9ec79dfd87d11305e824a35d9777d3
KelsGaming <alcorizamichael@gmail.com>
2023-06-10 21:30:39 +0800

    Resizing header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,9 +48,7 @@
                 <nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl " id="navbarBlur"
                 data-scroll="false">
                 <div class="container-fluid py-1 px-3">
-                    
-                    <h5 class="font-weight-bolder text-white mb-0">Dawn Amadis Christian School Inc.</h5>
-                    </nav>
+                    <h4 class="font-weight-bolder text-white mb-0">Dawn Amadis Christian School Inc.</h4>
                     <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
                     <div class="ms-md-auto pe-md-3 d-flex align-items-center">
                         <div class="input-group">
