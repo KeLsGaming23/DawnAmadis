3db005b982fc49de21be3438b498be840e81863c
KelsGaming <alcorizamichael@gmail.com>
2023-06-10 14:47:38 +0800

    Removing Search Bar and Login, Signup button

--- resources/views/parentdboard.blade.php
+++ resources/views/parentdboard.blade.php
@@ -47,15 +47,13 @@
           </div>
         </div>
       </div>
-      <div class="px-3 py-2 border-bottom mb-3">
-        <div class="container d-flex flex-wrap justify-content-center">
-          <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
-            <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
-          </form>
-
-          <div class="text-end">
-            <button type="button" class="btn btn-light text-dark me-2">Login</button>
-            <button type="button" class="btn btn-primary">Sign-up</button>
+      <div class="container-fluid pb-3">
+        <div class="d-grid gap-3" style="grid-template-columns: 1fr 2fr;">
+          <div class="bg-body-tertiary border rounded-3">
+            <br><br><br><br><br><br><br><br><br><br>
+          </div>
+          <div class="bg-body-tertiary border rounded-3">
+            <br><br><br><br><br><br><br><br><br><br>
           </div>
         </div>
       </div>
