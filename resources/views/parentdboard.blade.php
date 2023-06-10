253e014425947a62f327493cb55a15281a4484b7
KelsGaming <alcorizamichael@gmail.com>
2023-06-10 14:50:45 +0800

    Add favicons

--- resources/views/parentdboard.blade.php
+++ resources/views/parentdboard.blade.php
@@ -2,6 +2,32 @@
   <head>
     <title>Parent DashBoard</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style>
+      .form-control-dark {
+        border-color: var(--bs-gray);
+      }
+      .form-control-dark:focus {
+        border-color: #fff;
+        box-shadow: 0 0 0 .25rem rgba(255, 255, 255, .25);
+      }
+
+      .text-small {
+        font-size: 85%;
+      }
+
+      .dropdown-toggle:not(:focus) {
+        outline: 0;
+      }
+    </style>
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="manifest" href="/docs/5.3/assets/img/favicons/manifest.json">
+    <link rel="mask-icon" href="/docs/5.3/assets/img/favicons/safari-pinned-tab.svg" color="#712cf9">
+    <link rel="icon" href="/docs/5.3/assets/img/favicons/favicon.ico">
+    <meta name="theme-color" content="#712cf9">
+
   </head>
   <body>
     <header>
