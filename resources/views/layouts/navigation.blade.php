d7878571844484297eb5e68e3376fa8bf4839642
KelsGaming <alcorizamichael@gmail.com>
2023-07-22 15:10:38 +0800

    Add payment on navigation

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -45,7 +45,7 @@
               class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-app text-info text-sm opacity-10"></i>
             </div>
-            <span class="nav-link-text ms-1">Virtual Reality</span>
+            <span class="nav-link-text ms-1">Update Payment</span>
           </a>
         </li>
         <li class="nav-item mt-3">
