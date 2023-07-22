ec252b2851fe0f7f7a584bb50bf33c2ab7c7af03
KelsGaming <alcorizamichael@gmail.com>
2023-07-22 16:17:11 +0800

    Debugging

--- resources/views/studentInformation.blade.php
+++ resources/views/studentInformation.blade.php
@@ -72,7 +72,7 @@
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">{{$payment->$column}}</h5>
+        <h5 class="modal-title" id="exampleModalLabel">{{ $payment->$column }}</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
