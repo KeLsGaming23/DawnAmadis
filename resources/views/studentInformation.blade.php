ad93c8879fb76435d571a4949cc52bf9e050ea4f
KelsGaming <alcorizamichael@gmail.com>
2023-07-22 18:25:32 +0800

    payment id

--- resources/views/studentInformation.blade.php
+++ resources/views/studentInformation.blade.php
@@ -83,7 +83,7 @@
       </div>
       <div class="modal-body">
         <div class="card-body">
-          <p>{{ $student->child->id }}</p>
+          <p>{{ $payment->id }}</p>
           <form role="form text-left" id="editPaymentForm" action="{{ route('payment.edit', ['id' => 1, 'month' => '1st']) }}" method="POST">
             @csrf
             <input type="hidden" name="_method" value="PUT">
