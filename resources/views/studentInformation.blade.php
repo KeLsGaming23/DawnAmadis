ef91bcc4c70bdfe91c3725c7543bd100a920531b
KelsGaming <alcorizamichael@gmail.com>
2023-07-22 15:49:28 +0800

    Tidying modal info

--- resources/views/studentInformation.blade.php
+++ resources/views/studentInformation.blade.php
@@ -82,26 +82,12 @@
           <form role="form text-left">
             <label>Email</label>
             <div class="input-group mb-3">
-              <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="email-addon">
-            </div>
-            <label>Password</label>
-            <div class="input-group mb-3">
-              <input type="email" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password-addon">
-            </div>
-            <div class="form-check form-switch">
-              <input class="form-check-input" type="checkbox" id="rememberMe" checked="">
-              <label class="form-check-label" for="rememberMe">Remember me</label>
+              <input type="text" class="form-control" placeholder="Change Amount">
             </div>
             <div class="text-center">
               <button type="button" class="btn btn-round bg-gradient-info btn-lg w-100 mt-4 mb-0">Sign in</button>
             </div>
           </form>
-        </div>
-        <div class="card-footer text-center pt-0 px-lg-2 px-1">
-          <p class="mb-4 text-sm mx-auto">
-            Don't have an account?
-            <a href="javascript:;" class="text-info text-gradient font-weight-bold">Sign up</a>
-          </p>
         </div>
       </div>
       
